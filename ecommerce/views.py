b31d6b955d88e887f4db270af2bc0c80612f6c70
Ayşegül Rana Erdemli <aysegulrana@sabanciuniv.edu>
2021-04-12 22:06:32 +0300

    POST User API

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -14,3 +14,9 @@ class userList(APIView):
         user1 = user.objects.all()
         serializer = userSerializer(user1, many=True)
         return Response(serializer.data)
+    def post(self,request):
+        serializer = userSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
