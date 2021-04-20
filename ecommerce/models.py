4c3b9e65a862a356d3fd7f2ccecefd46da0a8fd1
Ayşegül Rana Erdemli <aysegulrana@sabanciuniv.edu>
2021-04-20 21:30:11 +0300

    funko model

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -15,17 +15,12 @@ class user(models.Model):
 class product(models.Model):
     id = models.AutoField(primary_key=True)
     product_name = models.CharField(max_length=50)
-    product_genre = models.CharField(max_length=30)
-    product_number = models.IntegerField()
+    product_model = models.CharField(max_length=30) #hangi dizi filme ait
+    product_size = models.IntegerField()
     product_description = models.TextField(max_length=300, null=True)
     product_stock = models.IntegerField()
     product_price = models.FloatField()
     warranty = models.CharField(max_length=50)
-    publisher = models.CharField(max_length=50)
-    page_number = models.IntegerField()
-    author = models.CharField(max_length=50)
+    seller = models.CharField(max_length=50)
     image = models.ImageField(null=True)
-    year = models.IntegerField()
-    translator = models.CharField(max_length=50, null=True)
-    editor = models.CharField(max_length=50, null=True)
 
