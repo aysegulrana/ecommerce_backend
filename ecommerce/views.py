e8bcddb3fe3f8d7960f164a7c1328ca9c44d0fd8
Ayşegül Rana Erdemli <aysegulrana@sabanciuniv.edu>
2021-06-11 02:26:39 +0300

    guncel

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.parsers import JSONParser
+from rest_framework.utils import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
